5525fb440b091ce495ff05c05fb4d537c293ad81
gbrlsg <gabrieldesouzaguarilha@gmail.com>
2024-09-20 12:10:05 -0300

    feat: working dashboard with filterable-by-input table

--- assets/javascript/App.js
+++ assets/javascript/App.js
@@ -1,72 +1,110 @@
 import React, { useState, useEffect } from 'react';
+import { 
+Table, Form, Container, Card, InputGroup, Button,  } from 'react-bootstrap';
+import { Search } from 'react-bootstrap-icons'
 
-const MonreqList = function(props) {
+const MonreqList = function({ monreqs, onFilterChange, filter }) {
     return (
-      <table className="table table-striped table-hover table-bordered table-light">
-        <thead>
-          <tr>
-            <th>Status</th>
-            <th>ID</th>
-            <th>Transportadora</th>
-            <th>Origem</th>
-            <th>Destino</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            props.monreqs.map((monreq, index) => {
-              return (
-                <tr key={index}>
-                  <td>{monreq.statusDisplay}</td>
-                  <td>{monreq.pk}</td>
-                  <td>{monreq.shippingCompany}</td>
-                  <td>{monreq.origin}</td>
-                  <td>{monreq.destination}</td>
-                 </tr>
-              );
-            })
-          }
-          </tbody>
-      </table>
+      <Container fluid className="mt-5">
+        <Card>
+          <Card.Header>
+            <InputGroup className='w-25'>
+              <Button variant='primary' type='button'>
+                <Search/>
+              </Button>
+              <Form.Control
+                className="focus-ring"
+                type='text' 
+                placeholder='Filtra SMs'
+                value={filter}
+                onChange={onFilterChange}
+                style={{'--bs-focus-ring-width': '0.05rem'}}
+              ></Form.Control>
+            </InputGroup>
+          </Card.Header>
+          <Card.Body className='p-0'>
+              <Table striped hover bordered>
+                <thead>
+                  <tr>
+                    <th>Status</th>
+                    <th>ID</th>
+                    <th>Transportadora</th>
+                    <th>Origem</th>
+                    <th>Destino</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {
+                  monreqs.map((monreq, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{monreq.statusDisplay}</td>
+                        <td>{monreq.pk}</td>
+                        <td>{monreq.shippingCompany}</td>
+                        <td>{monreq.origin}</td>
+                        <td>{monreq.destination}</td>
+                      </tr>
+                    );
+                  })
+                }
+                </tbody>
+              </Table>
+          </Card.Body>
+        </Card>
+      </Container>
     );
   };
 
 const MonreqsApplication = function({ apiClient }) {
-    // State variable to show whether we're loading data or not.
-    // Defaults to "true" to show a loading screen until we get our data from the API
     const [isLoading, setIsLoading] = useState(true);
-    // State variable where we'll save our list of employees
     const [monreqs, setMonreqs] = useState([]);
+    const [monreqsDisplayed, setMonreqsDisplayed] = useState([]);
+    const [filter, setFilter] = useState('');
   
-    // This effect will be called when the component mounts and fetch the data
-    // from our API
+    const handleFilterChange = (e) => {
+      setFilter(e.target.value);
+      if (!e.target.value) {
+        setMonreqsDisplayed(monreqs);
+      } else {
+        const filteredData = monreqs.filter((item) =>
+          Object.values(item).some((val) =>
+            val.toString().toLowerCase().includes(e.target.value.toLowerCase())
+          )
+        );    
+        setMonreqsDisplayed(filteredData);
+      }
+    
+    };
+
     useEffect(() => {
       apiClient.monitoringRequestsList()
         .then(data => {
           if (Array.isArray(data)) {
             setMonreqs(data);
+            setMonreqsDisplayed(data);
           } else {
             console.error('Unexpected data structure:', data);
             setMonreqs([]);
+            setMonreqsDisplayed([]);
           }
           setIsLoading(false);
         })
         .catch(error => {
           console.error('Error fetching data:', error);
           setMonreqs([]);
+          setMonreqsDisplayed([]);
           setIsLoading(false);
         });
     }, [apiClient]);
   
-    // Show a loading state if we haven't gotten data back yet
     if (isLoading) {
       return <p>Carregando SMs...</p>;
     }
-    // Show an "empty" state if we have no employees
+
     if (!monreqs || monreqs.length === 0) {
       return <p>Nenhuma SM encontrada!</p>;
     } else {
-      return <MonreqList monreqs={monreqs} />;
+      return <MonreqList monreqs={monreqsDisplayed} onFilterChange={handleFilterChange} filter={filter} />;
     }
   }
   
